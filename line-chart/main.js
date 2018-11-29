cc1fe7743147ed88867fd1103f9ab28e817ab50f
Carl Centola <carl.centola@gmail.com>
2018-11-28 19:50:48 -0500

    added sandbox folder and well commented demonstration

--- line-chart/main.js
+++ line-chart/main.js
@@ -1,57 +1,32 @@
-/* bar chart */
+const chart = new Chart({
+  element: document.querySelector('.chart-container1'),
+  data: [
+    [new Date(2016, 0, 1), 10],
+    [new Date(2016, 1, 1), 70],
+    [new Date(2016, 2, 1), 30],
+    [new Date(2016, 3, 1), 10],
+    [new Date(2016, 4, 1), 40]
+  ]
+});
 
-// SET PARAMETERS //////////////////////////////////////////////////////////////
+// change line colour on click
+d3.selectAll('button.color').on('click', function () {
+  const color = d3.select(this).text().split(' ')[0];
+  chart.setColor(color);
+});
 
-// define margins
-const margin = { left: 100, right: 10, top: 10, bottom: 100 };
+// change data on click to something randomly-generated
+d3.selectAll('button.data').on('click', () => {
+  chart.setData([
+    [new Date(2016, 0, 1), Math.random() * 100],
+    [new Date(2016, 1, 1), Math.random() * 100],
+    [new Date(2016, 2, 1), Math.random() * 100],
+    [new Date(2016, 3, 1), Math.random() * 100],
+    [new Date(2016, 4, 1), Math.random() * 100]
+  ]);
+});
 
-// define width and height
-const width = 600 - margin.left - margin.right;
-const height = 400 - margin.top - margin.bottom;
-
-// define canvas
-const g = d3
-  .select('#chart-area')
-  .append('svg')
-  .attr('width', width + margin.left + margin.right)
-  .attr('height', height + margin.top + margin.bottom)
-  .append('g')
-  .attr('transform', `translate(${margin.left},${margin.top})`);
-
-// x label
-const xLabel = g
-  .append('g')
-  .attr('class', 'x-axis-label')
-  .attr('x', width / 2)
-  .attr('y', height + 140)
-  .attr('font-size', '20px')
-  .attr('text-anchor', 'middle')
-  .text("The World's Tallest Buildings");
-
-// y label
-const yLabel = g
-  .append('g')
-  .attr('class', 'y-axis-label')
-  .attr('x', -(height / 2))
-  .attr('y', -60)
-  .attr('font-size', '20px')
-  .attr('text-anchor', 'middle')
-  .attr('transform', 'rotate(-90)')
-  .text('Height (m)');
-
-// DRAW ///////////////////////////////////////////////////////////////////////
-
-d3.csv('data/walmart.csv')
-  .then(data => {
-    // clean data
-    data.forEach(d => {
-      d.date = parseTime(d.date);
-      d.close = +d.close;
-    });
-
-    // log data to console
-    console.log(data);
-  })
-  .catch(error => {
-    console.log(error);
-  });
+// redraw chart on each resize
+// in a real-world example, it might be worth ‘throttling’ this
+// more info: http://sampsonblog.com/749/simple-throttle-function
+d3.select(window).on('resize', () => chart.draw());
