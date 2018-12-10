4680ad3b9c8f5465f37c0fa3ffbe683a480d670c
Carl Centola <carl.centola@gmail.com>
2018-12-10 15:39:12 -0500

    i made a mess today. adding grouped bar, KPIs, and better donut soon

--- donut-chart/main.js
+++ donut-chart/main.js
@@ -1,95 +1,153 @@
-// SET PARAMETERS //////////////////////////////////////////////////////////////
-// const svg = d3.select('svg').attr('width', 800).attr('height', 500);
-// const margin = { top: 20, right: 20, bottom: 30, left: 50 };
-// const width = +svg.attr('width') - margin.left - margin.right;
-// const height = +svg.attr('height') - margin.top - margin.bottom;
+// // SET PARAMETERS //////////////////////////////////////////////////////////////
 
-const width = 500;
-const height = 500;
+// initial SVG, margins, width, and height
+const margin = { top: 20, right: 20, bottom: 20, left: 20 };
+const width = 500 - margin.left - margin.right;
+const height = 500 - margin.top - margin.bottom;
+
+const thickness = 40; // defines innerRadius
+const duration = 750;
+
 const radius = Math.min(width, height) / 2;
-
-// // grab svg 
-// const g = svg
-//   .append('g')
-//   .attr('transform', `translate(${margin.left},${margin.top})`);
-
-// // set sacles
-// const x = d3.scaleLinear().rangeRound([0, width]);
-// const y = d3.scaleLinear().rangeRound([height, 0]);
-// const color = d3.scaleOrdinal(d3.schemeSet1);
-
-// // initialize tooltip
-// const tip = d3.tip()
-//   .attr('class', 'd3-tip')
-//   .offset([-10, 0])
-//   .html(function (d) {
-//     return `
-//       <strong>Sepal Length: </strong> <span style='color:red'> ${d.SepalLengthCm}</span><br>
-//       <strong>Sepal Width: </strong> <span style='color:red'> ${d.SepalWidthCm}</span>
-//     `;
-//   })
-
-// svg.call(tip);
-
-
-
-const svg = d3
-  .select('#chart-area')
-  .append('svg')
-  .attr('width', width)
-  .attr('height', height)
-  .append('g')
-  .attr('transform', `translate(${width / 2}, ${height / 2})`);
-
 const color = d3.scaleOrdinal(d3.schemeSet1);
 
-const pie = d3
-  .pie()
-  .value(d => d.count)
-  .sort(null);
 
-const arc = d3
-  .arc()
-  .innerRadius(0)
+const data = [
+  { name: "USA", value: 40 },
+  { name: "UK", value: 20 },
+  { name: "Canada", value: 30 },
+  { name: "Mexico", value: 10 },
+];
+
+let text = ''; // needs to become a dynamic total added to data binding
+
+const svg = d3.select('body')
+  .append('svg')
+  .attr('class', 'pie')
+  .attr('width', width)
+  .attr('height', height);
+
+const g = svg.append('g')
+  .attr('transform', `translate(${width / 2}, ${height / 2})`);
+
+const arc = d3.arc()
+  .innerRadius(radius - thickness)
   .outerRadius(radius);
 
-function type(d) {
-  d.apples = Number(d.apples);
-  d.oranges = Number(d.oranges);
-  return d;
-}
+// const pie = d3.pie()
+//   .value(d => d.value)
+//   .sort(null);
 
-function arcTween(a) {
-  const i = d3.interpolate(this._current, a);
-  this._current = i(1);
-  return t => arc(i(t));
-}
+// const path = g.selectAll('path')
+//   .data(pie(data))
+//   .enter()
+//   .append("g")
+//   .on("mouseover", function (d) {
 
-d3.json('data/data.json', type).then(data => {
-  d3.selectAll('input').on('change', update);
+//     let g = d3.select(this)
+//       .style("cursor", "pointer")
+//       .style("fill", "black")
+//       .append("g")
+//       .attr("class", "text-group");
 
-  function update(val = this.value) {
-    // Join new data
-    const path = svg.selectAll('path').data(pie(data[val]));
+//     g.append("text")
+//       .attr("class", "name-text")
+//       .text(`${d.data.name}`)
+//       .attr('text-anchor', 'middle')
+//       .attr('dy', '-1.2em');
 
-    // Update existing arcs
-    path
-      .transition()
-      .duration(200)
-      .attrTween('d', arcTween);
+//     g.append("text")
+//       .attr("class", "value-text")
+//       .text(`${d.data.value}`)
+//       .attr('text-anchor', 'middle')
+//       .attr('dy', '.6em');
+//   })
+//   .on("mouseout", function (d) {
+//     d3.select(this)
+//       .style("cursor", "none")
+//       .style("fill", color(this._current))
+//       .select(".text-group").remove();
+//   })
+//   .append('path')
+//   .attr('d', arc)
+//   .attr('fill', (d, i) => color(i))
+//   .on("mouseover", function (d) {
+//     d3.select(this)
+//       .style("cursor", "pointer")
+//       .style("fill", "black");
+//   })
+//   .on("mouseout", function (d) {
+//     d3.select(this)
+//       .style("cursor", "none")
+//       .style("fill", color(this._current));
+//   })
+//   .each(function (d, i) { this._current = i; });
 
-    // Enter new arcs
-    path
-      .enter()
-      .append('path')
-      .attr('fill', (d, i) => color(i))
-      .attr('d', arc)
-      .attr('stroke', 'white')
-      .attr('stroke-width', '2px')
-      .each(function (d) {
-        this._current = d;
-      });
-  }
+// g.append('text')
+//   .attr('text-anchor', 'middle')
+//   .attr('dy', '.35em')
+//   .text(text);
 
-  update('apples');
-});
+d3.json('data/data.json').then(data => {
+
+  const pie = d3.pie()
+    .value(d => d.value)
+    .sort(null);
+
+  const path = g.selectAll('path')
+    .data(pie(data))
+    .enter()
+    .append("g")
+    .on("mouseover", function (d) {
+
+      let g = d3.select(this)
+        .style("cursor", "pointer")
+        .style("fill", "black")
+        .append("g")
+        .attr("class", "text-group");
+
+      g.append("text")
+        .attr("class", "name-text")
+        .text(`${d.data.name}`)
+        .attr('text-anchor', 'middle')
+        .attr('dy', '-1.2em');
+
+      g.append("text")
+        .attr("class", "value-text")
+        .text(`${d.data.value}`)
+        .attr('text-anchor', 'middle')
+        .attr('dy', '.6em');
+    })
+    .on("mouseout", function (d) {
+      d3.select(this)
+        .style("cursor", "none")
+        .style("fill", color(this._current))
+        .select(".text-group").remove();
+    })
+    .append('path')
+    .attr('d', arc)
+    .attr('fill', (d, i) => color(i))
+    .on("mouseover", function (d) {
+      d3.select(this)
+        .style("cursor", "pointer")
+        .style("fill", "black");
+    })
+    .on("mouseout", function (d) {
+      d3.select(this)
+        .style("cursor", "none")
+        .style("fill", color(this._current));
+    })
+    .each(function (d, i) { this._current = i; });
+
+  g.append('text')
+    .attr('text-anchor', 'middle')
+    .attr('dy', '.35em')
+    .text(text);
+
+
+  // log data
+  console.log(data);
+
+}).catch(error => {
+  console.log(error);
+})
